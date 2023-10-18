d249333389c53f7d550dcfb1f0dd2a7d8f993b96
marcoDmc <marcodamasceno07@gmail.com>
2023-10-18 03:56:13 -0300

    chore: config database and CORS

---
+++ Data/TaskSchedulingDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskScheduling.Mappings;
+using TaskScheduling.Models;
+
+namespace TaskScheduling.Data;
+
+public class TaskSchedulingDb : DbContext
+{
+    
+    public DbSet<TaskModel> Tasks { get; set; }
+
+    public TaskSchedulingDb(DbContextOptions options) : base(options)
+    {
+    }
+
+    public TaskSchedulingDb()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configRoot = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            //replace appsettings.Development.json with appsettings.json
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+        optionsBuilder.UseSqlServer(configRoot.GetConnectionString("DefaultConnection"));
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TaskMap());
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,36 @@
+using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Logging;
+using TaskScheduling.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TaskSchedulingDb>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "Open",
+        builder =>
+            builder
+                .SetIsOriginAllowed(_ => true)
+                .WithOrigins("http://localhost:5117")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+    );
+});
+
+// solving JsonException object cycle problem
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+IdentityModelEventSource.ShowPII = true;
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +44,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("Open");
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
