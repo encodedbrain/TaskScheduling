d72d760b24875f9ddb826719e5690b291fc078ae
marcoDmc <marcodamasceno07@gmail.com>
2023-10-18 03:52:55 -0300

    feat: added entity models

---
+++ Models/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskScheduling.Models;
+
+public class TaskModel
+{
+    public enum StatusTask
+    {
+        Pending = 0,
+        Complete = 1
+    }
+    
+    public int  Id { get; set; }
+
+    public TaskModel(string? title, string? description, DateTime date, StatusTask value)
+    {
+        Title = title;
+        Description = description;
+        Date = date;
+        Status = value;
+    }
+
+    public TaskModel()
+    {
+    }
+
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public StatusTask Status { get; set; }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using TaskScheduling.Interfaces;
+
+namespace TaskScheduling.Models;
+
+public class UserModel
+{
+    public UserModel(string nickname, string email, string password)
+    {
+        Nickname = nickname;
+        Email = email;
+        Password = password;
+    }
+
+    public UserModel()
+    {
+
+    }
+
+    public int  Id { get; set; }
+    public string? Nickname { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+
+    public virtual IEnumerable<TaskModel> Tasks { get; set; }
+}
