9de59b43d7c37f2429d85cf7c9da1c87f22302e9
marcoDmc <marcodamasceno07@gmail.com>
2023-10-18 03:56:37 -0300

    chore: added interface

---
+++ Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskScheduling.Interfaces;
+
+public interface IUser
+{
+    public bool VaLidateEmail(string email);
+
+    public bool ValidateNickname(string name);
+
+    public bool ValidatePassword(string password);
+
+    public string EncryptingPassword(string password);
+}
